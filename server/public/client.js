52976b79c0fef43a8798bd6a9b7abc6d102a4fec
Luke Johnson <lukejohnson3712@gmail.com>
2020-05-30 13:28:15 -0500

    Further client input validation.

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,8 @@
 $(document).ready(runTheseFunctions);
 
 const calculationsArray = [];
+const operationKeys = ['+', '-', 'x', 'X', '*', '/'];
+let canEnterAnotherPeriod = true;
 
 function runTheseFunctions() {
   // Check if the character the user is trying to enter are valid.
@@ -9,21 +11,35 @@ function runTheseFunctions() {
 }
 
 function checkIfInputIsValid(event) {
+  console.log(event.key);
   // Array of the string values for allowed keys.
   const allowedKeys = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspace',
     'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter'];
-  const operationKeys = ['+', '-', 'x', 'X', '*', '/'];
   // If the keyboard value matches any item in the allowedKeys array enteredAValidKey =
   // true.
-  const enteredOperator = operationKeys.some(allowedKey => event.key === allowedKey);
   // If the input is 'C' run the clear the input.
   if (event.key === 'c' || event.key === 'C') {
-    event.preventDefault();
     clearInputBox();
-  } else if (event.key === 'x' || event.key === 'X') {
+    calculationsArray.length = 0;
+    canEnterAnotherPeriod = true;
+    event.preventDefault();
+  } else if (event.key === '*' || event.key === 'x' || event.key === 'X') {
+    // The user may have entered '*', but treat it as if they entered 'x';
+    $('#inputNumbersField').val($('#inputNumbersField').val() + 'x');
     calculationsArray.push('x');
+    canEnterAnotherPeriod = false;
+    event.preventDefault();
+  // The user entered another operator (like +) so allow them to add another period.
+  } else if (operationKeys.includes(event.key)) {
+    canEnterAnotherPeriod = true;
+  } else if (event.key === '.') {
+    if (canEnterAnotherPeriod === true) {
+      canEnterAnotherPeriod = false;
+    } else {
+      event.preventDefault();
+    }
   } else {
-    const enteredAValidKey = allowedKeys.some(allowedKey => event.key === allowedKey);
+    const enteredAValidKey = allowedKeys.concat(operationKeys).some(allowedKey => event.key === allowedKey);
     if (enteredAValidKey === false) {
       event.preventDefault();
     } else {
@@ -34,7 +50,8 @@ function checkIfInputIsValid(event) {
 
 function calculate() {
   console.log(calculationsArray);
-  console.log();
+  const includesAnOperator = operationKeys.includes($('#inputNumbersField').val());
+  console.log(includesAnOperator);
 }
 
 function clearInputBox() {
