bc4c343fd3f135c340ff50a532717e09d8456827
Luke Johnson <lukejohnson3712@gmail.com>
2020-06-02 10:56:34 -0500

    Update order.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const calculationHistory = require('./public/calculations');
 
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5001 || process.env.PORT;
+const port = process.env.PORT || 5001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
