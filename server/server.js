f95c3de7bfa5f8bd5a7bc2996094b10ddaf7e723
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-21 12:19:36 -0500

    Add option to run on localhost 5000.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const calculationHistory = require('./public/calculations');
 
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
